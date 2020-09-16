4a12506372537abb1129ff358b6d7cd3b765e177
Andrei <vjandr3i@yahoo.com>
2020-09-16 12:03:21 +0200

    update js

--- app.js
+++ app.js
@@ -1,6 +1,28 @@
 let count = 77
 
-let container = document.getElementById('cars') 
-container.innerHTML = `<img src="https://tinyurl.com/ironhack-pokemons/${count}.svg">`
-
 // let , const ,  var 
+
+function render(){
+    let container = document.getElementById('cars') 
+    container.innerHTML = `<img class="poke" src="https://tinyurl.com/ironhack-pokemons/${count}.svg">`    
+}
+
+let prev = document.getElementById('prev')
+let next = document.getElementById('next')
+
+prev.onclick = function(){
+    if (count > 1){
+        count = count - 1
+        render()
+    }
+    
+}
+
+next.onclick = function(){
+    if (count < 649){
+    count = count + 1
+    render()
+    }
+}
+
+render()
